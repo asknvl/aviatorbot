272cc230f62427a1e5cd04861c34348982b9d736
Alexey Konovalov <asknvl@gmail.com>
2023-12-13 03:25:54 +0300

    wip

---
+++ aviatorbot/Views/loggerView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace aviatorbot.Views
+{
+    public partial class loggerView : UserControl
+    {
+        public loggerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
