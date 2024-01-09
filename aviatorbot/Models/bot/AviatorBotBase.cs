ff0c7583852528bfb2534e6007a5237268c1dcaf
Alexey Konovalov <asknvl@gmail.com>
2024-01-10 02:33:17 +0300

    wip

--- aviatorbot/Models/bot/AviatorBotBase.cs
+++ aviatorbot/Models/bot/AviatorBotBase.cs
@@ -411,45 +411,45 @@ namespace aviatorbot.Model.bot
 
             try
             {
-                if (message.Text != null)
-                {
-                    if (message.Text.Equals("/start"))
-                    {
-                        await sendOperatorTextMessage(op, chat, $"{op.first_name} {op.last_name}, вы вошли как оператор");
-                    }
+                //if (message.Text != null)
+                //{
+                //    if (message.Text.Equals("/start"))
+                //    {
+                //        await sendOperatorTextMessage(op, chat, $"{op.first_name} {op.last_name}, вы вошли как оператор");
+                //    }
 
-                    if (message.Text.Equals("/updatemessages"))
-                    {
-                        MessageProcessor.Clear();
-                        op.state = State.waiting_new_message;
-                        return;
-                    }
-                    if (message.Text.Equals("GIVE REG"))
-                    {
-                        await bot.SendTextMessageAsync(message.From.Id, "Введите TG id для установки статуса РЕГИСТРАЦИЯ:");
-                        op.state = State.waiting_reg_access;
-                        return;
-                    }
-                    if (message.Text.Equals("GIVE FD"))
-                    {
-                        await bot.SendTextMessageAsync(message.From.Id, "Введите TG id для установки статуса ФД:");
-                        op.state = State.waiting_fd_access;
-                        return;
-                    }
-                    if (message.Text.Equals("GIVE VIP"))
-                    {
-                        await bot.SendTextMessageAsync(message.From.Id, "Введите TG id для предоставления VIP:");
-                        op.state = State.waiting_vip_access;
-                        return;
-                    }
-                    if (message.Text.Equals("CHECK STATUS"))
-                    {
-                        await bot.SendTextMessageAsync(message.From.Id, "Введите TG ID для определения статуса:");
-                        op.state = State.waiting_check_status_by_tg_id;
-                        return;
-                    }                  
+                //    if (message.Text.Equals("/updatemessages"))
+                //    {
+                //        MessageProcessor.Clear();
+                //        op.state = State.waiting_new_message;
+                //        return;
+                //    }
+                //    if (message.Text.Equals("GIVE REG"))
+                //    {
+                //        await bot.SendTextMessageAsync(message.From.Id, "Введите TG id для установки статуса РЕГИСТРАЦИЯ:");
+                //        op.state = State.waiting_reg_access;
+                //        return;
+                //    }
+                //    if (message.Text.Equals("GIVE FD"))
+                //    {
+                //        await bot.SendTextMessageAsync(message.From.Id, "Введите TG id для установки статуса ФД:");
+                //        op.state = State.waiting_fd_access;
+                //        return;
+                //    }
+                //    if (message.Text.Equals("GIVE VIP"))
+                //    {
+                //        await bot.SendTextMessageAsync(message.From.Id, "Введите TG id для предоставления VIP:");
+                //        op.state = State.waiting_vip_access;
+                //        return;
+                //    }
+                //    if (message.Text.Equals("CHECK STATUS"))
+                //    {
+                //        await bot.SendTextMessageAsync(message.From.Id, "Введите TG ID для определения статуса:");
+                //        op.state = State.waiting_check_status_by_tg_id;
+                //        return;
+                //    }                  
 
-                }
+                //}
 
                 if (state == State.waiting_new_message)
                 {
@@ -458,144 +458,105 @@ namespace aviatorbot.Model.bot
                     return;
                 }
 
-                switch (op.state)
-                {
-                    //case State.waiting_new_message:
-                    //    MessageProcessor.Add(AwaitedMessageCode, message, PM);
-                    //    state = State.free;
-                    //    break;
+                //switch (op.state)
+                //{
+                //    //case State.waiting_new_message:
+                //    //    MessageProcessor.Add(AwaitedMessageCode, message, PM);
+                //    //    state = State.free;
+                //    //    break;
 
-                    case State.waiting_reg_access:
-                        try
-                        {
-                            long tg_id = long.Parse(message.Text);
-                            string uuid = string.Empty;
-                            string status = string.Empty;
-                            (uuid, status) = await server.GetFollowerState(Geotag, tg_id);
+                //    case State.waiting_reg_access:
+                //        try
+                //        {
+                //            long tg_id = long.Parse(message.Text);
+                //            string uuid = string.Empty;
+                //            string status = string.Empty;
+                //            (uuid, status) = await server.GetFollowerState(Geotag, tg_id);
 
-                            await server.SetFollowerRegistered(uuid);
+                //            await server.SetFollowerRegistered(uuid);
 
-                            string msg = $"Пользователю {tg_id} установлен статус ЗАРЕГИСТРИРОВАН";
-                            await sendOperatorTextMessage(op, chat, msg);
-                            logger.inf(geotag, msg);
+                //            string msg = $"Пользователю {tg_id} установлен статус ЗАРЕГИСТРИРОВАН";
+                //            await sendOperatorTextMessage(op, chat, msg);
+                //            logger.inf(geotag, msg);
 
-                        }
-                        catch (Exception ex)
-                        {
-                            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
-                        } finally
-                        {
-                            state = State.free;
-                        }
-                        break;
+                //        }
+                //        catch (Exception ex)
+                //        {
+                //            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
+                //        } finally
+                //        {
+                //            state = State.free;
+                //        }
+                //        break;
 
-                    case State.waiting_fd_access:
-                        try
-                        {
-                            long tg_id = long.Parse(message.Text);
-                            string uuid = string.Empty;
-                            string status = string.Empty;
-                            (uuid, status) = await server.GetFollowerState(Geotag, tg_id);
+                //    case State.waiting_fd_access:
+                //        try
+                //        {
+                //            long tg_id = long.Parse(message.Text);
+                //            string uuid = string.Empty;
+                //            string status = string.Empty;
+                //            (uuid, status) = await server.GetFollowerState(Geotag, tg_id);
 
-                            await server.SetFollowerMadeDeposit(uuid);
+                //            await server.SetFollowerMadeDeposit(uuid);
 
-                            string msg = $"Пользователю {tg_id} установлен статус ФД";
-                            await sendOperatorTextMessage(op, chat, msg);
-                            logger.inf(geotag, msg);
+                //            string msg = $"Пользователю {tg_id} установлен статус ФД";
+                //            await sendOperatorTextMessage(op, chat, msg);
+                //            logger.inf(geotag, msg);
 
-                        }
-                        catch (Exception ex)
-                        {
-                            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
-                        } finally
-                        {
-                            state = State.free;
-                        }
-                        break;
+                //        }
+                //        catch (Exception ex)
+                //        {
+                //            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
+                //        } finally
+                //        {
+                //            state = State.free;
+                //        }
+                //        break;
 
-                    case State.waiting_vip_access:
-                        try
-                        {
-                            long tg_id = long.Parse(message.Text);
-                            string uuid = string.Empty;
-                            string status = string.Empty;
-                            (uuid, status) = await server.GetFollowerState(Geotag, tg_id);
+                //    case State.waiting_vip_access:
+                //        try
+                //        {
+                //            long tg_id = long.Parse(message.Text);
+                //            string uuid = string.Empty;
+                //            string status = string.Empty;
+                //            (uuid, status) = await server.GetFollowerState(Geotag, tg_id);
 
-                            if (!string.IsNullOrEmpty(uuid))
-                            {
-                                switch (status)
-                                {
-                                    case "WREG":
-                                        await server.SetFollowerRegistered(uuid);
-                                        await server.SetFollowerMadeDeposit(uuid);
-                                        await server.SetFollowerMadeDeposit(uuid);
-                                        break;
-                                    case "WFDEP":
-                                        await server.SetFollowerMadeDeposit(uuid);
-                                        await server.SetFollowerMadeDeposit(uuid);
-                                        break;
-                                    case "WREDEP1":
-                                        await server.SetFollowerMadeDeposit(uuid);
-                                        break;
-                                    case "WREDEP2":
-                                        break;
-                                    default:
-                                        return;
-                                }
+                //            if (!string.IsNullOrEmpty(uuid))
+                //            {
+                //                switch (status)
+                //                {
+                //                    case "WREG":
+                //                        await server.SetFollowerRegistered(uuid);
+                //                        await server.SetFollowerMadeDeposit(uuid);
+                //                        await server.SetFollowerMadeDeposit(uuid);
+                //                        break;
+                //                    case "WFDEP":
+                //                        await server.SetFollowerMadeDeposit(uuid);
+                //                        await server.SetFollowerMadeDeposit(uuid);
+                //                        break;
+                //                    case "WREDEP1":
+                //                        await server.SetFollowerMadeDeposit(uuid);
+                //                        break;
+                //                    case "WREDEP2":
+                //                        break;
+                //                    default:
+                //                        return;
+                //                }
 
-                                string msg = $"Пользователю {tg_id} предоставлен доступ к каналу";
-                                await sendOperatorTextMessage(op, chat, msg);
-                                logger.inf(geotag, msg);
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
-                        } finally
-                        {
-                            state = State.free;
-                        }
-                        break;
-
-                    case State.waiting_check_status_by_tg_id:
-                        try
-                        {
-                            long tg_id = long.Parse(message.Text);                            
-                            var resp = await server.GetFollowerStateResponse(Geotag, tg_id);
-
-                            string text_status = "";
-
-                            switch (resp.status_code)
-                            {
-                                case "WREG":
-                                    text_status = "Не зарегистрирован";
-                                    break;
-                                case "WFDEP":
-                                    text_status = "Ожидается ФД";
-                                    break;
-                                default:
-                                    if (resp.status_code.Contains("WREDEP"))
-                                    {
-                                        text_status = $"Ожидается редепозит {resp.status_code.Replace("WREDEP", "")}";
-                                    }
-                                    break;
-                            }
-
-                            string affId = (!string.IsNullOrEmpty(resp.player_id)) ? $", id в ПП: {resp.player_id}" : "";
-
-                            string msg = $"Cтатус пользователя `{tg_id}`: {text_status} {affId}";
-                            await sendOperatorTextMessage(op, chat, msg);
-                            logger.inf(geotag, msg);
-                        }
-                        catch (Exception ex)
-                        {
-                            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
-                        } finally
-                        {
-                            state = State.free;
-                        }
-                        break;
-                }
+                //                string msg = $"Пользователю {tg_id} предоставлен доступ к каналу";
+                //                await sendOperatorTextMessage(op, chat, msg);
+                //                logger.inf(geotag, msg);
+                //            }
+                //        }
+                //        catch (Exception ex)
+                //        {
+                //            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
+                //        } finally
+                //        {
+                //            state = State.free;
+                //        }
+                //        break;                   
+                //}
             }
             catch (Exception ex)
             {
--- aviatorbot/Models/bot/infoBot.cs
+++ aviatorbot/Models/bot/infoBot.cs
@@ -46,7 +46,25 @@ namespace aviatorbot.Models.bot
                     ResizeKeyboard = true,
                     OneTimeKeyboard = false,
                 };
+            } else
+                 if (op.permissions.Any(p => p.type.Equals(OperatorPermissionType.set_user_status)) || op.permissions.Any(p => p.type.Equals(OperatorPermissionType.all)))
+            {
+
+                replyKeyboardMarkup = new(new[]
+                        {
+                            new KeyboardButton[] { $"INFO BY TG ID" },
+                            new KeyboardButton[] { $"INFO BY PLAYER ID" },
+                            new KeyboardButton[] { $"GIVE REG" },
+                            new KeyboardButton[] { $"GIVE FD" },
+                            new KeyboardButton[] { $"GIVE VIP" },
+                            new KeyboardButton[] { $"CHECK STATUS" }
+                        })
+                {
+                    ResizeKeyboard = true,
+                    OneTimeKeyboard = false,
+                };
             }
+
             await bot.SendTextMessageAsync(
                 chat,
                 text: text,
@@ -55,7 +73,7 @@ namespace aviatorbot.Models.bot
         }
 
         public override Task processFollower(Message message)
-        {            
+        {
             return Task.CompletedTask;
         }
 
@@ -86,11 +104,32 @@ namespace aviatorbot.Models.bot
                         op.state = State.waiting_check_status_by_player_id;
                         return;
                     }
+
+                    if (message.Text.Equals("GIVE REG"))
+                    {
+                        await bot.SendTextMessageAsync(message.From.Id, "Введите TG id для установки статуса РЕГИСТРАЦИЯ:");
+                        op.state = State.waiting_reg_access;
+                        return;
+                    }
+
+                    if (message.Text.Equals("GIVE FD"))
+                    {
+                        await bot.SendTextMessageAsync(message.From.Id, "Введите TG id для установки статуса ФД:");
+                        op.state = State.waiting_fd_access;
+                        return;
+                    }
+
+                    if (message.Text.Equals("GIVE VIP"))
+                    {
+                        await bot.SendTextMessageAsync(message.From.Id, "Введите TG id для предоставления VIP:");
+                        op.state = State.waiting_vip_access;
+                        return;
+                    }
                 }
 
                 switch (op.state)
                 {
-                    
+
                     case State.waiting_check_status_by_tg_id:
                         try
                         {
@@ -116,7 +155,7 @@ namespace aviatorbot.Models.bot
                         {
                             await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
                         }
-                        catch (Exception ex) 
+                        catch (Exception ex)
                         {
                             await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
                         } finally
@@ -138,17 +177,113 @@ namespace aviatorbot.Models.bot
                                 {
                                     await bot.SendTextMessageAsync(message.From.Id, item.ToString());
 
-                                } catch (Exception ex)
+                                }
+                                catch (Exception ex)
                                 {
 
-                                } 
+                                }
                             }
 
-                        } catch (Exception ex)
+                        }
+                        catch (Exception ex)
                         {
-                            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");                            
+                            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
                         }
                         break;
+
+                    case State.waiting_reg_access:
+                        try
+                        {
+                            long tg_id = long.Parse(message.Text);
+                            string uuid = string.Empty;
+                            string status = string.Empty;
+                            (uuid, status) = await server.GetFollowerState(Geotag, tg_id);
+
+                            await server.SetFollowerRegistered(uuid);
+
+                            string msg = $"Пользователю {tg_id} установлен статус ЗАРЕГИСТРИРОВАН";
+                            await sendOperatorTextMessage(op, chat, msg);
+                            logger.inf(Geotag, msg);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
+                        } finally
+                        {
+                            state = State.free;
+                        }
+                        break;
+
+                    case State.waiting_fd_access:
+                        try
+                        {
+                            long tg_id = long.Parse(message.Text);
+                            string uuid = string.Empty;
+                            string status = string.Empty;
+                            (uuid, status) = await server.GetFollowerState(Geotag, tg_id);
+
+                            await server.SetFollowerMadeDeposit(uuid);
+
+                            string msg = $"Пользователю {tg_id} установлен статус ФД";
+                            await sendOperatorTextMessage(op, chat, msg);
+                            logger.inf(Geotag, msg);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
+                        } finally
+                        {
+                            state = State.free;
+                        }
+                        break;
+
+                    case State.waiting_vip_access:
+                        try
+                        {
+                            long tg_id = long.Parse(message.Text);
+                            string uuid = string.Empty;
+                            string status = string.Empty;
+                            (uuid, status) = await server.GetFollowerState(Geotag, tg_id);
+
+                            if (!string.IsNullOrEmpty(uuid))
+                            {
+                                switch (status)
+                                {
+                                    case "WREG":
+                                        await server.SetFollowerRegistered(uuid);
+                                        await server.SetFollowerMadeDeposit(uuid);
+                                        await server.SetFollowerMadeDeposit(uuid);
+                                        break;
+                                    case "WFDEP":
+                                        await server.SetFollowerMadeDeposit(uuid);
+                                        await server.SetFollowerMadeDeposit(uuid);
+                                        break;
+                                    case "WREDEP1":
+                                        await server.SetFollowerMadeDeposit(uuid);
+                                        break;
+                                    case "WREDEP2":
+                                        break;
+                                    default:
+                                        return;
+                                }
+
+                                string msg = $"Пользователю {tg_id} предоставлен доступ к каналу";
+                                await sendOperatorTextMessage(op, chat, msg);
+                                logger.inf(Geotag, msg);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            await bot.SendTextMessageAsync(message.From.Id, $"{ex.Message}");
+                        } finally
+                        {
+                            state = State.free;
+                        }
+                        break;
+
+
                 }
             }
             catch (Exception ex)
