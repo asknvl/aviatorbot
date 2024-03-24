d54f94e3ab5e7358bd7a4b5a1a45dd55eed4526a
Alexey Konovalov <asknvl@gmail.com>
2024-03-24 19:30:02 +0300

    wip

--- aviatorbot/Models/messages/_1wLinkGenerator.cs
+++ aviatorbot/Models/messages/_1wLinkGenerator.cs
@@ -10,22 +10,22 @@ namespace botservice.Models.messages
     {
         public static string getRegUrl(string link, string uuid)
         {
-            return $"{link}/casino/play/aviator/list?open=register&sub1={uuid}";
+            return $"{link}/casino/list?open=register&sub1={uuid}";
         }
 
         public static string getFDUrl(string link, string uuid)
         {
-            return $"{link}/casino/play/aviator/list?open=deposit&sub1={uuid}";
+            return $"{link}/casino/list?open=deposit&sub1={uuid}";
         }
 
         public static string getGameUrl(string link)
         {
-            return $"{link}/casino/play/aviator/list?open=deposit";
+            return $"{link}/casino/list?open=deposit";
         }
 
         public static string getFriendUrl(string link)
         {
-            return $"{link}/casino/play/aviator/list?open=register&sub1=friend";
+            return $"{link}/casino/list?open=register&sub1=friend";
         }
     }
 }
